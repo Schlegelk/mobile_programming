eee28441899cfc1952229d51caffccf1f1f686e0
unknown <samuelrichard0309@gmail.com>
2024-09-11 00:15:15 +0700

    theme: light mode & dark mode

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+  surface: Colors.black,
+  primary: Colors.black,
+  secondary: Colors.black,
+  tertiary: Colors.black,
+  inversePrimary: Colors.grey.shade300,
+));
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Colors.white,
+  primary: Colors.white,
+  secondary: Colors.white,
+  tertiary: Colors.white,
+  inversePrimary: Colors.white,
+));
