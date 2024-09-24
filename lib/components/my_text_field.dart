50a1406a7875677ba0697eed0cbcf45e196698e2
abigailega <abigail.ega@gmail.com>
2024-09-24 22:28:32 +0700

    535230202 abigail

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+/*
+TEXT FIELD
+
+tempat (kotak) untuk user mengisi email atau password
+
+*/
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText:
+          obscureText, //untuk pengisian pada password sehingga berubah menjadi **** sewaktu diketik
+      decoration: InputDecoration(
+        // border sewaktu tidak di klik atau dalam kondisi tidak ingin diisi
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+
+        // border sewaktu di klik atau dalam kondisi diisi
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+
+        // untuk warna hinttext dalam box
+        fillColor: Theme.of(context).colorScheme.secondary,
+        filled: true,
+        hintText: hintText,
+        hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+      ),
+    );
+  }
+}
