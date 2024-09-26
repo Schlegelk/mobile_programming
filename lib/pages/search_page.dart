2e5a7ea0ab7f2687e9e1a6062abd9cfcdeaef301
axel <belaxist@gmail.com>
2024-09-26 21:45:55 +0700

    search page to search users

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final _searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: "Search for a user",
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+    );
+    ;
+  }
+}
